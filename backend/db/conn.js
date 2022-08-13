aca091ee0c4158bc437c5e83a322bf9f6080f08e
itsharsh334 <80212334+itsharsh334@users.noreply.github.com>
2022-08-13 17:06:45 +0530

    Update conn.js

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 // const DB = "process.env.DATABASE";
 const DB =
-  "mongodb+srv://harsh:Sherlock@cluster0.9wlpo.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://name:password@cluster0.9wlpo.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose
   .connect(DB, {
